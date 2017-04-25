ca7678f235701287f02e136a1e0e08530691047d
matyaz96 <matiasnicolas.soto@alumnos.ulagos.cl>
2017-04-25 09:24:01 -0300

    Update CPUPlanning.cpp

--- CPUPlanning.cpp
+++ CPUPlanning.cpp
@@ -24,6 +24,8 @@ void CPUPlanning::read_file() {
         process.id = id;
         process.runtime = runtime;
         process.arrival = arrival;
+        process.running = 0;
+        process.waiting = 0;
         process_g.push_back(process);
     }
 }
@@ -46,11 +48,144 @@ void CPUPlanning::run() {
 void CPUPlanning::run_fcfs() {
     cout << "Calculando First-Come-First-Served" << endl;
     // Este método calcular e imprimir en pantalla el throughput, turnaround y response time
+    deque<Process> active_processes;
+    deque<Process> inactive_processes;
+    Process current_process ={-1,-1,-1};
+    float total_processes = process_g.size();
+    while(process_g.size()>0 or active_processes.size()>0 or current_process.id>-1){
+        //Check if some process arrive
+        vector<int> ids_to_remove;
+        for(int i=0; i<process_g.size();i++){
+            if(process_g[i].arrival==current_time_g){
+                active_processes.push_back(process_g[i]);
+                ids_to_remove.push_back(process_g[i].id);
+            }
+        }
+        // Remove active processes from g
+        for(auto id:ids_to_remove){
+            int pos=0;
+            for(int i=0; i<process_g.size();i++){
+                if(process_g[i].id==id){
+                    pos = i;
+                }
+            }
+            process_g.erase(process_g.begin()+pos);
+        }
+
+        // Set current process
+        if(current_process.id == -1) {
+            current_process = active_processes.front();
+            active_processes.pop_front();
+        }
+
+        // Run the current process
+        current_process.running += 1;
+
+        // Check if the process needs more time
+        if (current_process.running == current_process.runtime){
+            inactive_processes.push_back(current_process);
+            current_process.id = -1;
+        }
+
+        //Update waiting times inside the queue
+        for(int i=0; i<active_processes.size();i++)
+            active_processes[i].waiting++;
+
+
+        // update the time
+        current_time_g++;
+    }
+
+    // Throughput
+    cout << "Troughput: " << total_processes/current_time_g << endl;
+
+    // Turnaround
+    float total_time=0;
+    for(auto p:inactive_processes)
+    {
+        total_time+=p.runtime + p.waiting;
+    }
+    cout << "Turnaround: " << total_time/total_processes << endl;
+
+    // Waiting time
+    total_time=0;
+    for(auto p:inactive_processes)
+    {
+        total_time+= p.waiting;
+    }
+    cout << "Response time: " << total_time/total_processes << endl;
+
 }
 
 void CPUPlanning::run_sjf() {
     cout << "Calculando Short-Job-First" << endl;
     // Este método calcular e imprimir en pantalla el throughput, turnaround y response time
+    deque<Process> active_processes;
+    deque<Process> inactive_processes;
+    Process current_process ={-1,-1,-1};
+    float total_processes = process_g.size();
+    while(process_g.size()>0 or active_processes.size()>0 or current_process.id>-1){
+        //Check if some process arrive
+        vector<int> ids_to_remove;
+        for(int i=0; i<process_g.size();i++){
+            if(process_g[i].arrival==current_time_g){
+                active_processes.push_back(process_g[i]);
+                ids_to_remove.push_back(process_g[i].id);
+            }
+        }
+        // Remove active processes from g
+        for(auto id:ids_to_remove){
+            int pos=0;
+            for(int i=0; i<process_g.size();i++){
+                if(process_g[i].id==id){
+                    pos = i;
+                }
+            }
+            process_g.erase(process_g.begin()+pos);
+        }
+
+        // Set current process
+        if(current_process.id == -1) {
+            current_process = active_processes.front();
+            active_processes.pop_front();
+        }
+
+        // Run the current process
+        current_process.running += 1;
+
+        // Check if the process needs more time
+        if (current_process.running == current_process.runtime){
+            inactive_processes.push_back(current_process);
+            current_process.id = -1;
+        }
+
+        //Update waiting times inside the queue
+        for(int i=0; i<active_processes.size();i++)
+            active_processes[i].waiting++;
+
+
+        // update the time
+        current_time_g++;
+    }
+
+    // Throughput
+    cout << "Troughput: " << total_processes/current_time_g << endl;
+
+    // Turnaround
+    float total_time=0;
+    for(auto p:inactive_processes)
+    {
+        total_time+=p.runtime + p.waiting;
+    }
+    cout << "Turnaround: " << total_time/total_processes << endl;
+
+    // Waiting time
+    total_time=0;
+    for(auto p:inactive_processes)
+    {
+        total_time+= p.waiting;
+    }
+    cout << "Response time: " << total_time/total_processes << endl;
 }
 
 void CPUPlanning::run_sjf_p() {
